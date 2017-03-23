9defbf04cd48b192966bdcb5c4bc9de235ba5946
uzaycetin <uzay00@gmail.com>
2017-03-23 21:02:08 +0300

    really!!

--- hungry.py
+++ hungry.py
@@ -1,7 +1,5 @@
 answer = input("Are you hungry?")
 if answer == "yes" or answer == "y":
     print("Eat something!")
-    print("This mistake will be committed ")
 else:
     print("Really?")
-    print("Another mistake")
